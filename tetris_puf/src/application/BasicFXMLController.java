876baa67a6caba75c75d5561efcb8ef7617c78ef
tetris <tetris@frieda-Mint>
2020-11-05 18:04:43 +0100

    more rudimental layout

--- tetris_puf/src/application/BasicFXMLController.java
+++ tetris_puf/src/application/BasicFXMLController.java
@@ -2,39 +2,67 @@ package application;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.geometry.HPos;
+import javafx.geometry.VPos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TitledPane;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.Region;
 
 public class BasicFXMLController {
+	private int cellWidth = 12;
+	private int cellHeight = 12;
 	
 	@FXML
-	private GridPane grid;
+	private GridPane playingGrid, nextUpGrid;
+	
+	@FXML
+	private Label pointsLabel;
+	
+	@FXML
+	private TitledPane pointsTitle, nextUpTitle;
 
     public void initialize() {
     	buildUpGridAndAddPanels();
+    	buildUpGridForNextUpShape();
+    	buildUpLabelForPoints();
     }   
     
     @FXML
+    private void buildUpLabelForPoints() {
+    	pointsTitle.getStyleClass().add("nextUpBackground");
+    	pointsLabel.getStyleClass().add("pointsBackground");
+    }
+    
+    @FXML
     private void buildUpGridAndAddPanels() {
-
-    	for(int i=0; i<55; i++) {
-    		for (int j=0; j<70; j++) {
+    	
+    	nextUpTitle.getStyleClass().add("nextUpBackground");
+    	buildUpGrid(50,60,playingGrid);
+    }
+    
+    @FXML
+    private void buildUpGridForNextUpShape() {
+    	buildUpGrid(5,5,nextUpGrid);
+    }
+    
+    private void buildUpGrid(int numberOfColumns, int numberOfRows, GridPane grid) {
+    
+    	for(int i=0; i<numberOfColumns; i++) {
+    		for (int j=0; j<numberOfRows; j++) {
     			Pane pane = new Pane();
     			pane.getStyleClass().add("game-grid-cell");
-    			pane.setPrefSize(10, 10);
+    			pane.setPrefSize(cellWidth, cellHeight);
     			grid.setMaxSize(Region.USE_COMPUTED_SIZE, Region.USE_COMPUTED_SIZE);
 
-//    			pane.getChildren().add(new Label(String.valueOf(i) + "," + String.valueOf(j)));
     			GridPane.setRowIndex(pane, j);
     	    	GridPane.setColumnIndex(pane, i);
     			grid.getChildren().add(pane);
     		}
     	}
-    	grid.setGridLinesVisible(true);
-
     }
 
 }
