dd13741878b39ea6cdd9bf231bb3ec37632eba8c
tetris <tetris@frieda-Mint>
2020-12-09 17:35:24 +0100

    add commit push

--- tetris_puf/src/application/Main.java
+++ tetris_puf/src/application/Main.java
@@ -6,7 +6,7 @@ import javafx.scene.Scene;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.stage.Stage;
-// REYKO LÖSCHENGIT TEST
+// REYKO LÖSCHENGIT
 public class Main extends Application {
 
 	@Override
